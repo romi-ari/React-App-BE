b695e2c041aefa4e43feb6f7ffd98696f76dc592
romiari <romiari777@gmail.com>
2022-11-01 22:20:02 +0700

    react-app

--- app/controllers/api/v1/GoogleLoginOrRegister.js
+++ app/controllers/api/v1/GoogleLoginOrRegister.js
@@ -1,7 +1,56 @@
+// const axios = require("axios");
+// const jwt = require("jsonwebtoken");
+// const { userGoogle } = require("../../../models");
+// const { OAuth2Client } = require("google-auth-library")
+// const client = new
+// OAuth2Client(process.env.GOOGLE_CLIENT_ID)
+// const JWT_SIGNATURE_KEY  = process.env;
+
+
+// function createToken(user) {
+//   const payload = {
+//     id: user.id,
+//     name: user.name,
+//     email: user.email,
+//   };
+
+//   return jwt.sign(payload, JWT_SIGNATURE_KEY);
+// }
+
+// async function handleGoogleLoginOrRegister(req, res) {
+//   const { accessToken } = req.body;
+//   const options = { headers: { Authorization: `Bearer ${accessToken}` } };
+
+//   try {
+//     const response = await axios.get(
+//       "https://www.googleapis.com/oauth2/v2/userinfo",
+//       options
+//     );
+//     // const ticket = await client.verifyIdToken({
+//     //   idToken: token,
+//     //   audience: process.env.REACT_APP_GOOGLE_CLIENT_ID
+//     // })
+//     const { id, email, name } = response.data;
+    
+//     let user = await userGoogle.findOne({ where: { googleId: id } });
+//     if (!user) user = await userGoogle.create({ email, name });
+
+//     const accessToken = createToken(user);
+
+//     res.status(201).json({ accessToken });
+//   } catch (err) {
+//     res.status(401).json({ error: { name: err.name, message: err.message } });
+//   }
+// }
+
+// module.exports = handleGoogleLoginOrRegister;
+
 const axios = require("axios");
 const jwt = require("jsonwebtoken");
 const { userGoogle } = require("../../../models");
-const JWT_SIGNATURE_KEY  = process.env;
+const { OAuth2Client } = require("google-auth-library")
+const { JWT_SECRET_KEY = "Rahasia" } = process.env;
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
 
 function createToken(user) {
   const payload = {
@@ -10,21 +59,23 @@ function createToken(user) {
     email: user.email,
   };
 
-  return jwt.sign(payload, JWT_SIGNATURE_KEY);
+  return jwt.sign(payload, JWT_SECRET_KEY);
 }
 
 async function handleGoogleLoginOrRegister(req, res) {
-  const { accessToken } = req.body;
-  const options = { headers: { Authorization: `Bearer ${accessToken}` } };
+  const { token } = req.body;
 
   try {
-    const response = await axios.get(
-      "https://www.googleapis.com/oauth2/v2/userinfo",
-      options
-    );
-    const { id, email, name } = response.data;
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID
+    })
 
-    let user = await userGoogle.findOne({ where: { googleId: id } });
+    const { email,name } = ticket.getPayload();
+
+    console.log(ticket.getPayload())
+
+    let user = await userGoogle.findOne({ where: { email: email } });
     if (!user) user = await userGoogle.create({ email, name });
 
     const accessToken = createToken(user);
@@ -36,3 +87,4 @@ async function handleGoogleLoginOrRegister(req, res) {
 }
 
 module.exports = handleGoogleLoginOrRegister;
+
